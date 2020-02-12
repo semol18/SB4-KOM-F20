8f7597473795ffed8f21d38bc1d16d956d44c1e9
Sebastian <sebastian@moldt.dk>
2020-02-12 13:46:46 +0100

    Bullets added
    Player can shoot with spacebar.
    Enemy shoots randomly.

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/gamestates/PlayState.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/gamestates/PlayState.java
@@ -1,20 +1,25 @@
 package dk.sdu.mmmi.cbse.gamestates;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import dk.sdu.mmmi.cbse.entities.Bullet;
 import dk.sdu.mmmi.cbse.entities.Enemy;
 import dk.sdu.mmmi.cbse.entities.Player;
 import dk.sdu.mmmi.cbse.managers.GameKeys;
 import dk.sdu.mmmi.cbse.managers.GameStateManager;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PlayState extends GameState {
 	
 	private ShapeRenderer sr;
 	private Player player;
+	private ArrayList<Bullet> playerBullets;
+	private ArrayList<Bullet> enemyBullets;
 	private Enemy enemy;
 	private Random rd = new Random();
-	private int enemyMovement;
+	private int enemyMovementTimer;
+	private int enemyShootTimer;
 	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
@@ -23,27 +28,61 @@ public class PlayState extends GameState {
 	public void init() {
 		
 		sr = new ShapeRenderer();
-		
-		player = new Player();
 
-		enemy = new Enemy();
+		playerBullets = new ArrayList<Bullet>();
+		player = new Player(playerBullets);
+
+		enemyBullets = new ArrayList<Bullet>();
+		enemy = new Enemy(enemyBullets);
 		
 	}
 	
 	public void update(float dt) {
-		
+
+		// Get user input.
 		handleInput();
-		
+
+		// Update player.
 		player.update(dt);
 
+		// Update player bullets.
+		for (int i = 0; i < playerBullets.size(); i++) {
+			playerBullets.get(i).update(dt);
+			if (playerBullets.get(i).shouldRemove()) {
+				playerBullets.remove(i);
+				i--;
+			}
+		}
+
+		// Update enemy.
 		enemy.update(dt);
-		
+
+		// Update enemy bullets.
+		for (int i = 0; i < enemyBullets.size(); i++) {
+			enemyBullets.get(i).update(dt);
+			if (enemyBullets.get(i).shouldRemove()) {
+				enemyBullets.remove(i);
+				i--;
+			}
+		}
 	}
 	
 	public void draw() {
+
+		// Draw player.
 		player.draw(sr);
 
+		// Draw player bullets.
+		for (int i = 0; i < playerBullets.size(); i++) {
+			playerBullets.get(i).draw(sr);
+		}
+
+		// Draw enemy.
 		enemy.draw(sr);
+
+		for (int i = 0; i < enemyBullets.size(); i++) {
+			enemyBullets.get(i).draw(sr);
+		}
 	}
 	
 	public void handleInput() {
@@ -54,19 +93,31 @@ public class PlayState extends GameState {
 		player.setUp(GameKeys.isDown(GameKeys.UP));
 		player.setDown(GameKeys.isDown(GameKeys.DOWN));
 
-		// Enemy movement.
-		if (enemyMovement == 0) {
-			enemy.setRight(rd.nextBoolean());
-			enemy.setUp(false);
-			enemyMovement = 50;
+		// Player shoot.
+		if(GameKeys.isPressed(GameKeys.SPACE)) {
+			player.shoot();
 		}
 
-		if (enemyMovement == 25) {
+		// Enemy movement.
+		if (enemyMovementTimer == 0) {
+			enemy.setRight(rd.nextBoolean());
+			enemy.setUp(false);
+			enemyMovementTimer = 50;
+		}
+
+		if (enemyMovementTimer == 25) {
 			enemy.setLeft(rd.nextBoolean());
 			enemy.setUp(false);
 		}
-		enemyMovement --;
+		enemyMovementTimer --;
 		enemy.setUp(rd.nextBoolean());
+
+		// Enemy shoot.
+		if (enemyShootTimer == 0) {
+			enemy.shoot();
+			enemyShootTimer = 60;
+		}
+		enemyShootTimer --;
 	}
 	
 	public void dispose() {}
